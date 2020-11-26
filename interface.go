3780fb2f7982a0f0a797aa7b45a55eb5739a209a
jgzhang <jgzhang@jgzhangdeMacBook-Pro.local>
2020-11-26 10:03:48 +0800

    add query order status

--- interface.go
+++ interface.go
@@ -1,6 +1,7 @@
 package main
 
 import (
+	"fmt"
 	"github.com/fireagainsmile/fabric-chaincodes/components"
 	"github.com/hyperledger/fabric-contract-api-go/contractapi"
 )
@@ -20,8 +21,17 @@ func (s *SimpleContract)ListOrders() string {
 	return s.or.ID
 }
 
-func (s *SimpleContract)ConfirmOrder(res, id string)  {
-	s.or = nil
+func (s *SimpleContract)GetStatus() string {
+	return s.or.CurrentState.Name()
+}
+
+func (s *SimpleContract)ConfirmOrder(id string)  {
+	if s.or.ID == id{
+		s.or = nil
+		fmt.Println(id, ":Order Confirmed")
+	}else {
+		fmt.Println("no match order found")
+	}
 }
 
 func (s *SimpleContract)ServeOder(op, message string) error {
