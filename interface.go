abc0fd9427cfe5c52cbfad2ee3265eeb59bbf675
jgzhang <jgzhang@jgzhangdeMacBook-Pro.local>
2020-11-26 09:36:17 +0800

    debug

--- interface.go
+++ interface.go
@@ -7,52 +7,25 @@ import (
 
 type SimpleContract struct {
 	contractapi.Contract
-	org *components.Org
-	users *components.User
-}
-
-// admin interfaces
-func (s *SimpleContract)InitWithUser(name string) error {
-	s.org = components.DefaultOrg
-	s.users = components.NewUser(name)
-	return nil
-}
-
-func (s *SimpleContract)RegisterRestaurant(name string) {
-	s.org.RegisterRestaurant(name)
-}
-
-func (s *SimpleContract)UpdateRestaurant(openning bool, id string)  {
-	s.org.UpdateRestaurant(openning, id)
-}
-
-func (s *SimpleContract)ListRestaurant()  string{
-	return s.org.ListRestaurant()
-}
-
-func (s *SimpleContract)GetRestaurantName(id string) string  {
+	or *components.OrderEvent
 }
 
 //user interfaces
-func (s *SimpleContract)CommitOrder(want ,resId string) error {
-	return s.users.CommitOrder(want, resId)
+func (s *SimpleContract)CommitOrder(want string) error {
+	 s.or = components.NewOrderEvent(want)
+	return nil
 }
 
 func (s *SimpleContract)ListOrders() string {
-	return s.users.ListOrders()
+	return s.or.ID
 }
 
 func (s *SimpleContract)ConfirmOrder(res, id string)  {
-	s.users.ConfirmOrder(res, id)
-}
-
-// restaurant interfaces
-func (s *SimpleContract)QueryOderInfo() (string,error) {
-	return  s.Name, nil
+	s.or = nil
 }
 
 func (s *SimpleContract)ServeOder(op, message string) error {
-	s.org.ServeOrder(op, message)
+	s.or.HandleEvent(op, message)
 	return nil
 }
 
